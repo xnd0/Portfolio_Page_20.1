2aeb0b5e7f566046519e1816d7e50cc0ee2fea67
xndcmd <xndcmd@gmail.com>
2022-08-01 16:24:03 -0700

    update src folder w/ new page structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
