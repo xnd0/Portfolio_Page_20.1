02dac69bd265ed13e4d4ee29a872d306ff4c2c31
xndcmd <xndcmd@gmail.com>
2022-08-01 18:54:58 -0700

    adjusted styling

--- public/portfolio-page/src/components/Footer.js
+++ public/portfolio-page/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <div>
-          <h6>-Footer Area- Built by Xnd0 -Footer Area-</h6>
+          <h6>○○ Footer Area __Built_by_xnd0_2022__ Footer Area ○○</h6>
         </div>
     )
 };
--- public/portfolio-page/src/components/pages/About.js
+++ public/portfolio-page/src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import pic from './2Space_Surfer.png'
 
 export default function About() {
   return (
@@ -7,15 +8,9 @@ export default function About() {
       <p>
         I am a developer. Welcome to my gitHub Portfolio page.
       </p>
-      <p>
-      More Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris, ut interdum nunc egestas sed.
-      </p>
-      <p>
-      Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper imperdiet.
-      </p>
-      <p>
-      Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <img src= {pic} alt="spaceman surfing"   />
+     <br></br>
+     <br></br>
     </div>
   );
 }
